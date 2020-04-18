c2b4e70878aef2ead13bf7ec16dd4b79a3f05d92
Pavel T <ptokarevskiy@ya.ru>
2020-04-18 15:12:21 +0300

    30 - Final

--- AppStore/Controller/Today/TodayController.swift
+++ AppStore/Controller/Today/TodayController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     let cellId = "cellId"
+    var appFullscreenController: UIViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +35,15 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let redView = UIView()
-        redView.backgroundColor = .red
+//        let redView = UIView()
+//        redView.backgroundColor = .red
+        let appFullscreenController = AppFullscreenController()
+        let redView = appFullscreenController.view!
         redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
         redView.layer.cornerRadius = 16
         view.addSubview(redView)
+        addChild(appFullscreenController)
+        self.appFullscreenController = appFullscreenController
 //        redView.frame = .init(x: 0, y: 0, width: 100, height: 200)
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
         //absolute corners of cell
@@ -46,9 +51,15 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         
         self.startingFrame = startingframe
         redView.frame = startingframe
+        // why i don't use a transition delegate?
+        
+        // we're using frames for animation
+        // frames aren't reliable enough for animations
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
             redView.frame = self.view.frame
+            
+            self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
         }, completion: nil)
     }
     
@@ -60,9 +71,12 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
             if let startingFrame = self.startingFrame {
                 gesture.view?.frame = startingFrame
+                
+                self.tabBarController?.tabBar.transform = .identity
             }
         }) { _ in
             gesture.view?.removeFromSuperview()
+            self.appFullscreenController.removeFromParent()
         }
     }
 }
---
+++ AppStore/Views/Today/AppFullscreenHeaderCell.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class AppFullscreenHeaderCell: UITableViewCell {
+    
+    let todayCell = TodayCell()
+    let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "closeButton"), for: .normal)
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(todayCell)
+        todayCell.fillSuperview()
+        addSubview(closeButton)
+        closeButton.anchor(top: topAnchor,
+                           leading: nil,
+                           bottom: nil,
+                           trailing: trailingAnchor,
+                           padding: .init(top: 12, left: 0, bottom: 0, right: 12),
+                           size: .init(width: 80, height: 38))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
